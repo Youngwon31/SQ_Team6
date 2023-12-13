3dee07c19567015a8443273928aaf1e81f408033
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2023-12-13 03:48:19 -0500

    Add features and pages for when users forget their passwords..

---
+++ ForgotPassword.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ForgotPassword.aspx.cs" Inherits="Namespace.ForgotPassword" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Forgot Password</title>
+    <link href="Styles.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <label for="username">Username:</label>
+            <input type="text" id="username" runat="server" />
+        </div>
+        <div>
+            <label for="email">Email:</label>
+            <input type="text" id="email" runat="server" />
+        </div>
+        <button type="submit" runat="server" onserverclick="RetrievePassword_Click">Retrieve Password</button>
+    </form>
+    <div runat="server" id="passwordDisplay"></div>
+</body>
+</html>
---
+++ ForgotPassword.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web.UI;
+using MySql.Data.MySqlClient;
+
+namespace Namespace
+{
+    public partial class ForgotPassword : Page
+    {
+        protected void RetrievePassword_Click(object sender, EventArgs e)
+        {
+            string username = this.username.Value;
+            string email = this.email.Value;
+
+            string connectionString = "Server=localhost; Database=UserDB; Uid=root; Pwd=0000;";
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                string query = "SELECT password FROM users WHERE username = @username AND email = @email";
+
+                connection.Open();
+
+                using (var command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@email", email);
+
+                    object result = command.ExecuteScalar();
+                    if (result != null)
+                    {
+                        passwordDisplay.InnerText = $"Your password is: {result.ToString()}";
+                    }
+                    else
+                    {
+                        passwordDisplay.InnerText = "No matching user found.";
+                    }
+                }
+            }
+        }
+    }
+}
