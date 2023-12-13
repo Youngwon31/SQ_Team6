4d576608a2c4b72515a05555802b423c82048db0
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2023-12-13 01:49:05 -0500

    A file share with four features.
    1. Limit to Alphabetical, Numerical and Special Characters
    2. Password Strength Indicator
    3. Sign up Password Reveal Button
    4. Return To Login Page Button

---
+++ Sign-Up/SignUp.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace FreshSaver
+{
+    public partial class SignUp : System.Web.UI.Page
+    {
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            string username = this.username.Text;
+            string email = this.email.Text;
+            string password = this.password.Text;
+
+            // 서버 측 유효성 검사
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                errorLabel.Text = "Username is required.";
+                errorLabel.Visible = true;
+                return;
+            }
+            if (!Regex.IsMatch(username, @"^[a-zA-Z0-9]*$"))
+            {
+                errorLabel.Text = "Username can only contain letters and numbers.";
+                errorLabel.Visible = true;
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(email))
+            {
+                errorLabel.Text = "Email is required.";
+                errorLabel.Visible = true;
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                errorLabel.Text = "Password is required.";
+                errorLabel.Visible = true;
+                return;
+            }
+            if (!Regex.IsMatch(password, @"^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).+$"))
+            {
+                errorLabel.Text = "Password must contain at least one letter, one number, and one special character.";
+                errorLabel.Visible = true;
+                return;
+            }
+
+            // 데이터베이스 연결 및 사용자 정보 저장 시도
+            string connectionString = "Server=localhost; Database=UserDB; Uid=root; Pwd=0000;";
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                var cmd = new MySqlCommand("INSERT INTO users (username, email, password) VALUES (@username, @email, @password)", conn);
+                cmd.Parameters.AddWithValue("@username", username);
+                cmd.Parameters.AddWithValue("@email", email);
+                cmd.Parameters.AddWithValue("@password", password); // Consider hashing the password
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    // 등록 성공 메시지
+                    errorLabel.Text = "Registration successful!";
+                    errorLabel.Visible = true;
+                    // Response.Redirect("Login.aspx"); // 성공 시 로그인 페이지로 이동
+                }
+                catch (MySqlException ex)
+                {
+                    errorLabel.Text = $"An error occurred: {ex.Message}";
+                    errorLabel.Visible = true;
+                }
+            }
+        }
+        protected void ReturnToLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
