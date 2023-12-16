8dc681d2e4442c68d992ad620e2d500c2012a1bf
Youngwon31 <115308850+Youngwon31@users.noreply.github.com>
2023-12-15 23:43:33 -0500

    Created StoreDescription.aspx and made it redirect from Home.aspx to StoreDescription.aspx
    I'm going to make shopping cart db after this.

---
+++ StoreDescription.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="StoreDescription.aspx.cs" Inherits="FreshSaver.StoreDescription" %>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Store Description</title>
+    <link href="Styles.css" rel="stylesheet" type="text/css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="store-details">
+            <h1 id="lblStoreName" runat="server"></h1>
+            <asp:Image id="imgStoreImage" runat="server" />
+            <p id="lblAddress" runat="server"></p>
+            <h2>Menu</h2>
+            <div class="menu-item">
+                <h3 id="lblMenuName" runat="server"></h3>
+                <p id="lblDescription" runat="server"></p>
+                <p>Was: $<span id="lblOriginalPrice" runat="server"></span></p>
+                <p>Now: $<span id="lblDiscountedPrice" runat="server"></span></p>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ StoreDescription.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace FreshSaver
+{
+    public partial class StoreDescription : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Retrieve StoreID from the query string
+                if (Request.QueryString["StoreID"] != null)
+                {
+                    int storeId = int.Parse(Request.QueryString["StoreID"]);
+                    LoadStoreDetails(storeId);
+                }
+            }
+        }
+
+        private void LoadStoreDetails(int storeId)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["StoreDB"].ConnectionString;
+
+            // 'SqlConnection'을 'MySqlConnection'으로 변경합니다.
+            using (var conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                // 'SqlCommand'를 'MySqlCommand'로 변경합니다.
+                string query = @"SELECT s.StoreName, s.Address, s.ImageURL, m.MenuName, m.Description, m.OriginalPrice, m.DiscountedPrice
+                         FROM Stores s
+                         JOIN MenuItems m ON s.StoreID = m.StoreID
+                         WHERE s.StoreID = @StoreID
+                         ORDER BY m.OriginalPrice DESC
+                         LIMIT 1;";
+
+                using (var cmd = new MySqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@StoreID", storeId);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            lblStoreName.InnerText = reader["StoreName"].ToString(); // InnerText 사용
+                                                                                     // imgStoreImage 및 lblAddress 설정 코드는 동일하게 유지
+                            lblMenuName.InnerText = reader["MenuName"].ToString(); // InnerText 사용
+                            lblDescription.InnerText = reader["Description"].ToString(); // InnerText 사용
+                            lblOriginalPrice.InnerText = reader["OriginalPrice"].ToString(); // InnerText 사용
+                            lblDiscountedPrice.InnerText = reader["DiscountedPrice"].ToString(); // InnerText 사용
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
